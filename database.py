2e4e2360de102ad1b8d23e9646657322c27ce7d1
artemijvasilev <nyssanA04@gmail.com>
2024-10-03 14:20:38 +0300

    start fastAPI

---
+++ database.py
@@ -0,0 +1,9 @@
+import psycopg2 as psdb
+from psycopg2 import sql
+import uuid
+import bcrypt
+import os
+from loguru import logger
+
+class Database:
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print()
+from fastapi import FastAPI, UploadFile, Form, Request, Response
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, PlainTextResponse, RedirectResponse, JSONResponse,FileResponse
+from database import Database
+from dotenv import load_dotenv
+import os
+
+app = FastAPI()
+
+load_dotenv("config.env")
+pg_host = os.getenv("PG_HOST")
+pg_port = os.getenv("PG_PORT")
+pg_user = os.getenv("PG_USER")
+pg_password = os.getenv("PG_PASSWORD")
+pg_database = os.getenv("PG_DB")
+db = Database()
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to FastAPI project with Database"}
