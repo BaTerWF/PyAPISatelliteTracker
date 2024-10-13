9a6b5c97759fb4da5725c990337220eb4f53d9c7
artemijvasilev <nyssanA04@gmail.com>
2024-10-13 13:38:00 +0300

    Fast API and class TLE add endpoin for get tle and select satellite

--- TLE.py
+++ TLE.py
@@ -5,11 +5,8 @@ from astropy import units as u
 from astropy.coordinates import CartesianRepresentation
 import numpy as np
 
-<<<<<<< HEAD
 
 # Пока не работает.
-=======
->>>>>>> b4c8a73 (Fast API and class TLE (beta))
 class TLEConverter:
     def __init__(self, tle_line1, tle_line2):
         self.tle_line1 = tle_line1
@@ -18,20 +15,12 @@ class TLEConverter:
         self.satellite = EarthSatellite(tle_line1, tle_line2, 'Sat', self.timescale)
 
     def tle_to_ef(self, time_str):
-<<<<<<< HEAD
-=======
-        """Преобразует TLE в Earth-Fixed координаты на заданное время"""
->>>>>>> b4c8a73 (Fast API and class TLE (beta))
         t = self.timescale.utc(Time(time_str).datetime)
         geocentric = self.satellite.at(t)
         subpoint = geocentric.subpoint()
         return subpoint.latitude.degrees, subpoint.longitude.degrees, subpoint.elevation.m
 
     def ef_to_j2000(self, time_str):
-<<<<<<< HEAD
-=======
-        """Преобразует Earth-Fixed в J2000"""
->>>>>>> b4c8a73 (Fast API and class TLE (beta))
         lat, lon, elevation = self.tle_to_ef(time_str)
         location = EarthLocation(lat=lat*u.deg, lon=lon*u.deg, height=elevation*u.m)
         time = Time(time_str)
@@ -40,7 +29,6 @@ class TLEConverter:
         return gcrs.cartesian
 
     def j2000_to_gtsk(self, time_str):
-<<<<<<< HEAD
         cartesian = self.ef_to_j2000(time_str)
         x, y, z = cartesian.x.value, cartesian.y.value, cartesian.z.value
         return x, y, z
@@ -49,20 +37,6 @@ class TLEConverter:
         return x, y, z
 
     def convert(self, time_str):
-=======
-        """Преобразует координаты J2000 в ГЦСК (ECEF)"""
-        cartesian = self.ef_to_j2000(time_str)
-        x, y, z = cartesian.x.value, cartesian.y.value, cartesian.z.value
-        # Простое преобразование в ГЦСК может быть сложным, но это начальный подход
-        return x, y, z
-
-    def gtsk_to_pz90(self, x, y, z):
-        """Преобразование из ГЦСК в ПЗ-90"""
-        return x, y, z
-
-    def convert(self, time_str):
-        """Основной метод для выполнения всех преобразований"""
->>>>>>> b4c8a73 (Fast API and class TLE (beta))
         lat, lon, elevation = self.tle_to_ef(time_str)
         print(f"EF (Lat, Lon, Elev): {lat}, {lon}, {elevation}")
 
