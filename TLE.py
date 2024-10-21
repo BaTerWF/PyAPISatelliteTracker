f250817327e5a41153c0b08b888d0e030066492a
artemijvasilev <nyssanA04@gmail.com>
2024-10-21 21:48:15 +0300

    Add map and bad lines

--- TLE.py
+++ TLE.py
@@ -1,50 +1,73 @@
-from skyfield.api import EarthSatellite, load
+from skyfield.api import load, EarthSatellite
 from astropy.time import Time
-from astropy.coordinates import EarthLocation, GCRS, ITRS, AltAz
+from astropy.coordinates import EarthLocation, GCRS, ITRS
 from astropy import units as u
-from astropy.coordinates import CartesianRepresentation
 import numpy as np
+from datetime import datetime, timedelta
 
 
-# Пока не работает.
 class TLEConverter:
     def __init__(self, tle_line1, tle_line2):
-        self.tle_line1 = tle_line1
-        self.tle_line2 = tle_line2
+        # Загружаем данные TLE и создаем объект спутника
         self.timescale = load.timescale()
-        self.satellite = EarthSatellite(tle_line1, tle_line2, 'Sat', self.timescale)
+        self.satellite = EarthSatellite(tle_line1, tle_line2, 'Satellite', self.timescale)
 
-    def tle_to_ef(self, time_str):
-        t = self.timescale.utc(Time(time_str).datetime)
+    def tle_to_ef(self, time):
+        """Преобразует TLE в Earth-Fixed координаты на заданное время"""
+        # Преобразуем время в формат Skyfield
+        t = self.timescale.utc(time.year, time.month, time.day, time.hour, time.minute, time.second)
         geocentric = self.satellite.at(t)
         subpoint = geocentric.subpoint()
         return subpoint.latitude.degrees, subpoint.longitude.degrees, subpoint.elevation.m
 
-    def ef_to_j2000(self, time_str):
-        lat, lon, elevation = self.tle_to_ef(time_str)
-        location = EarthLocation(lat=lat*u.deg, lon=lon*u.deg, height=elevation*u.m)
-        time = Time(time_str)
-        itrs = ITRS(location.get_itrs(obstime=time))
-        gcrs = itrs.transform_to(GCRS(obstime=time))
+    def ef_to_j2000(self, time):
+        """Преобразует Earth-Fixed в J2000"""
+        lat, lon, elevation = self.tle_to_ef(time)
+        location = EarthLocation(lat=lat * u.deg, lon=lon * u.deg, height=elevation * u.m)
+        time_astropy = Time(time.isoformat())
+        itrs = ITRS(location.get_itrs(obstime=time_astropy))
+        gcrs = itrs.transform_to(GCRS(obstime=time_astropy))
         return gcrs.cartesian
 
-    def j2000_to_gtsk(self, time_str):
-        cartesian = self.ef_to_j2000(time_str)
-        x, y, z = cartesian.x.value, cartesian.y.value, cartesian.z.value
-        return x, y, z
+    def calculate_orbit(self, start_time, end_time, step_minutes=1):
+        """Функция для расчета орбиты спутника за указанный временной период.
+        :param start_time: время начала (datetime)
+        :param end_time: время окончания (datetime)
+        :param step_minutes: шаг в минутах между расчетными точками
+        :return: список координат спутника на каждый временной шаг
+        """
+        # Преобразуем время начала и конца в формат Skyfield
+        t0 = self.timescale.utc(start_time.year, start_time.month, start_time.day, start_time.hour, start_time.minute)
+        t1 = self.timescale.utc(end_time.year, end_time.month, end_time.day, end_time.hour, end_time.minute)
 
-    def gtsk_to_pz90(self, x, y, z):
-        return x, y, z
+        # Вычисляем количество временных шагов
+        total_minutes = (end_time - start_time).total_seconds() / 60
+        num_steps = int(total_minutes // step_minutes)
 
-    def convert(self, time_str):
-        lat, lon, elevation = self.tle_to_ef(time_str)
+        # Создаем временной диапазон с заданным шагом
+        time_range = [t0 + (i * (t1 - t0) / num_steps) for i in range(num_steps + 1)]
+        orbit_coords = []
+
+        # Для каждого временного шага рассчитываем положение спутника
+        for t in time_range:
+            geocentric = self.satellite.at(t)
+            subpoint = geocentric.subpoint()
+            latitude = subpoint.latitude.degrees
+            longitude = subpoint.longitude.degrees
+            elevation = subpoint.elevation.m
+            orbit_coords.append((latitude, longitude, elevation))
+
+        return orbit_coords
+
+    def convert(self, time):
+        """Основной метод для выполнения всех преобразований на указанное время"""
+        lat, lon, elevation = self.tle_to_ef(time)
         print(f"EF (Lat, Lon, Elev): {lat}, {lon}, {elevation}")
 
-        j2000_coords = self.ef_to_j2000(time_str)
+        j2000_coords = self.ef_to_j2000(time)
         print(f"J2000 (X, Y, Z): {j2000_coords.x.value}, {j2000_coords.y.value}, {j2000_coords.z.value}")
 
-        x, y, z = self.j2000_to_gtsk(time_str)
-        print(f"GTSK (X, Y, Z): {x}, {y}, {z}")
+        # Здесь можно добавить дальнейшие преобразования, если необходимо
 
-        pz90_x, pz90_y, pz90_z = self.gtsk_to_pz90(x, y, z)
-        print(f"PZ-90 (X, Y, Z): {pz90_x}, {pz90_y}, {pz90_z}")
+        return lat, lon, elevation, j2000_coords
+
